526499e177c8ce7b3a1404074ee33e351fb2c216
Chinedu Asogwa <111493495+Asogwaoscar@users.noreply.github.com>
2022-10-21 02:31:31 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ let dev = {
 }
 console.log("His name is" + " " + dev.name + " " + "from" + " " + dev.location + " " + "who is" + " " + dev.age + " " + "of age and a" + " " + dev.stack + " " + "with" + " " + dev.exp + " " + "experience and also loves" + " " + dev.hobby);
 
-
+//Assignment
 // a program that displays good when even numbers are inserted
 
 
@@ -33,9 +33,35 @@ else{
 // Assignment 
 // write a program that sums up two inputs from a user and displays the answer in an alert box
 
-let person = "oscar";
-cloth = 100;
-shoe = 200;
+// As a string
 
-total = cloth + shoe;
-alert (total);
+var oscar = (e, d);
+ var e = prompt("enter a number")
+ var d = prompt("enter a number")
+var sum = (e) + (d);
+
+alert(sum);
+
+// As a number
+var oscar = (u, v);
+ var u = prompt("enter a number")
+ var v = prompt("enter a number")
+var sum = Number (u) + Number (v);
+
+alert(sum);
+
+//Assignment
+//write a program to check if the user is an adult or a minor
+
+var age = prompt("please enter your age");
+
+if (age < 1){
+    console.log("User is infant")
+}
+
+else if (age >= 1 && age <= 17){
+    console.log("User is a minor")
+}
+else{
+    console.log("User is an adult")
+};
